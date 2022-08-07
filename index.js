be9b4c3d9f47f58948df51d6d46f5989c9bd8f99
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-07 17:45:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat () {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat () {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    console.log(cats.pop())
+}
+function destructivelyRemoveFirstCat() {
+    console.log (cats.shift())
+}
+let feline = cats.concat (["Broom"])
+
+function appendCat() {
+    return feline
+} 
+const copyOfCats = [...cats]
+copyOfCats.unshift("Arnold")
+function prependCat () {
+    
+    return copyOfCats
+}
+const  newCopy = [...cats]
+function removeLastCat () {
+    console.log(newCopy.pop())
+    return newCopy
+}
+const lastCopy = [...cats]
+function removeFirstCat () {
+    console.log (lastCopy.shift())
+    return lastCopy
+}
+
+
